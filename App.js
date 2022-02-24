7f4cc102833bfac6ee2ea0dcac4ce485dd88a270
Akshay <apple@Apples-MacBook-Pro.local>
2022-02-24 15:24:42 +0530

    first commit

--- App.js
+++ App.js
@@ -1,12 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import { 
+  NavigationContainer, 
+  DefaultTheme as NavigationDefaultTheme,
+  DarkTheme as NavigationDarkTheme
+} from '@react-navigation/native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+
+//--this is the sidebar here
+import MainTabScreen from './screens/MainTabScreen'; 
+import {DrawerContent} from './screens/DrawerContent'; 
+import DetailsScreen from './screens/DetailsScreen'; 
+import SupportScreen from './screens/SupportScreen'; 
+import BookmarkScreen from './screens/BookmarkScreen'; 
+import SettingsScreen from './screens/SettingsScreen'; 
+import { LogBox } from "react-native";
+
+
+const Drawer = createDrawerNavigator();
+LogBox.ignoreAllLogs(true);
+
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Drawer.Navigator drawerContent={props => <DrawerContent {...props} />}>
+        <Drawer.Screen name="HomeDrawer" component={MainTabScreen} />
+        <Drawer.Screen name="SupportScreen" component={SupportScreen} />
+        <Drawer.Screen name="SettingsScreen" component={SettingsScreen} />
+        <Drawer.Screen name="BookmarkScreen" component={BookmarkScreen} />
+      </Drawer.Navigator>
+  </NavigationContainer>
   );
 }
 
